164c1b2e489288c4987f2e2266c9f8d2c1b93a4c
jafoor <abujafor.cse11@gmail.com>
2021-01-16 23:01:48 +0600

    Creating serializer basic

--- restapi/urls.py
+++ restapi/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('stuinfo/<int:pk>', views.student_detail),
+    path('stuinfo/', views.all_students),
 ]
